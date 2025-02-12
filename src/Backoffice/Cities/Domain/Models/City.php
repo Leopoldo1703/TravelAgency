e8d89244ed9c4bac3dc9a6027e7d734e34579b99
Leopoldo1703 <leopoldo.perez@lightit.io>
2025-02-12 12:55:11 -0300

    feat: Models and migrations

--- src/Backoffice/Cities/Domain/Models/City.php
+++ src/Backoffice/Cities/Domain/Models/City.php
@@ -8,6 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Lightit\Backoffice\Flights\Domain\Models\Flight;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Flight> $arrivals
+ * @property-read int|null $arrivals_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Flight> $departures
+ * @property-read int|null $departures_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class City extends Model
 {
     /**
--- src/Backoffice/Flights/Domain/Models/Flight.php
+++ src/Backoffice/Flights/Domain/Models/Flight.php
@@ -9,6 +9,33 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Lightit\Backoffice\Airlines\Domain\Models\Airline;
 use Lightit\Backoffice\Cities\Domain\Models\City;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $airline_id
+ * @property int $origin_id
+ * @property int $destination_id
+ * @property string $departure
+ * @property string $arrival
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Airline $airline
+ * @property-read City|null $destinationCity
+ * @property-read City|null $originCity
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereAirlineId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereArrival($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereDeparture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereDestinationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereOriginId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Flight whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Flight extends Model
 {
     /**
